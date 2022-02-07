a2542a943b05f7e8c5195da600ef401b3793f9bf
Jamie Bresch <hclin@ucar.edu>
2022-02-07 09:51:58 -0700

    explicitly set cmake flags and options from jcsda-Cheyenne-Intel.cmake (#121)
    TYPE: bug fix
    
    KEYWORDS: intel-impi toolchain
    
    DESCRIPTION OF CHANGES:
    The file /glade/work/miesch/jedi/jedi-cmake/cmake/Toolchains/jcsda-Cheyenne-Intel.cmake is no longer valid.
    While jcsda-Cheyenne-Intel.cmake is available in mpas-bundle/jedicmake/cmake/Toolchains/jcsda-Cheyenne-Intel.cmake after ecbuild, intel-impi-cheyenne.csh needs to be run before ecbuild.
    One option is to place jcsda-Cheyenne-Intel.cmake in another common directory.
    This PR proposes another option that is to expand the cmake flags explicitly in the set-env scripts.
    
    LIST OF MODIFIED FILES:
    M env-setup/intel-impi-cheyenne.csh
    M env-setup/intel-impi-cheyenne.sh
    
    TESTS CONDUCTED:
    mpas-jedi ctests on Cheyenne with jedi/intel-impi passed.

--- env-setup/intel-impi-cheyenne.sh
+++ env-setup/intel-impi-cheyenne.sh
@@ -7,7 +7,8 @@ module load json
 module load json-schema-validator
 unalias ecbuild
 ecb=`which ecbuild`
-alias ecbuild="$ecb  --toolchain=/glade/work/miesch/jedi/jedi-cmake/cmake/Toolchains/jcsda-Cheyenne-Intel.cmake"
+#alias ecbuild="$ecb  --toolchain=/glade/work/miesch/jedi/jedi-cmake/cmake/Toolchains/jcsda-Cheyenne-Intel.cmake"
+alias ecbuild="$ecb -DCMAKE_CXX_FLAGS='-gxx-name=/glade/u/apps/ch/opt/gnu/9.1.0/bin/g++' -DCMAKE_EXE_LINKER_FLAGS='-gxx-name=/glade/u/apps/ch/opt/gnu/9.1.0/bin/g++ -Wl,-rpath,/glade/u/apps/ch/opt/gnu/9.1.0/lib64' -DCMAKE_SHARED_LINKER_FLAGS='-gxx-name=/glade/u/apps/ch/opt/gnu/9.1.0/bin/g++ -Wl,-rpath,/glade/u/apps/ch/opt/gnu/9.1.0/lib64' -DENABLE_AEC=OFF -DFYPP_NO_LINE_NUMBERING=ON"
 #export LOCAL_PATH_JEDI_TESTFILES=/glade/u/home/maryamao/JEDI_test_files
 git lfs install
 ulimit -s unlimited
