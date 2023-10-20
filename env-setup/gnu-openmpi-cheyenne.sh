e69c3989733a9893f13298b8cf64b18a1932fdc7
Byoung-Joo (BJ) Jung <33296274+byoung-joo@users.noreply.github.com>
2023-10-20 08:44:12 -0600

    Adapt a new test data download logic (#155)
    TYPE: maintenance
    
    KEYWORDS: testdata, CMake
    
    DESCRIPTION OF CHANGES: This PR adapts a new test data download logic in JEDI. See more details in JCSDA-internal/jedi-bundle#71 , and related PRs including JCSDA-internal/mpas-jedi#923 . For mpas-jedi, it only works with ENABLE_MPAS_JEDI_DATA is set ON, but the tarball for mpas-jedi testdata can be prepared at some time.
    
    This PR was tested on Cheyenne with updating the spack-stack version from 1.4.0 to 1.5.0 .
    
    LIST OF MODIFIED FILES:
    M CMakeLists.txt
    M env-setup/gnu-openmpi-cheyenne.csh
    M env-setup/gnu-openmpi-cheyenne.sh
    
    TESTS CONDUCTED: Tested on Cheyenne. Mpas-bundle build was successful and all mpas-jedi ctests passed.

--- env-setup/gnu-openmpi-cheyenne.sh
+++ env-setup/gnu-openmpi-cheyenne.sh
@@ -1,5 +1,5 @@
 #
-echo "Loading Spack-Stack 1.4.0"
+echo "Loading Spack-Stack 1.5.0"
 #
 source /etc/profile.d/modules.sh  # note: needed on non-computing nodes
 module purge
@@ -9,11 +9,11 @@ module load miniconda/3.9.12
 module load ecflow/5.8.4
 module load mysql/8.0.31
 
-module use /glade/work/epicufsrt/contrib/spack-stack/spack-stack-1.4.0/envs/unified-env-v2/install/modulefiles/Core
+module use /glade/work/epicufsrt/contrib/spack-stack/cheyenne/spack-stack-1.5.0/envs/unified-env/install/modulefiles/Core
 module load stack-gcc/10.1.0
 module load stack-openmpi/4.1.1
 module load stack-python/3.9.12
-module load jedi-mpas-env/unified-dev
+module load jedi-mpas-env/1.0.0
 module list
 
 ulimit -s unlimited
